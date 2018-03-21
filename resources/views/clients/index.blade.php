6021e808c3bf73aa6e0d43b0bba46672bd201c2b
Oscar Diaz <od@backlayer.com.co>
2018-03-21 14:57:49 -0500

    commit inicial

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app') 
+@section('content')
+<div class="container">
+		<div class="row">
+			<div class="col-sm-7 offset-sm-3">
+				<br>
+				<br>
+				<br>
+					<table class="table table-hover" id="buscar_resultados">
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Nombre</th>
+								<th>Email</th>
+								<th>Pais</th>
+								<th>Fecha Creado</th>
+							</tr>
+						</thead>
+						<tbody>
+							
+						</tbody>
+					</table>
+			</div>
+		</div>		
+	</div>
+@endsection
